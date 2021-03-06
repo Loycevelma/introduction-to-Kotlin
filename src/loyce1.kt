ac79a03a21d87c62c470c82da9e7c62ce4ff78a9
loycevelma <amondiloyce@gmail.com>
2021-03-06 09:33:02 +0300

    second assignment

---
+++ src/loyce1.kt
@@ -0,0 +1,26 @@
+fun main(){
+   name()
+    var result=modulus(35,6)
+    println (result)
+    add()
+        value()
+
+}
+fun name(){
+    println("hello loyce velma")
+}
+fun modulus(num1:Int,num2:Int):Int{
+    var modulus=num1%num2
+    return modulus
+}
+fun add (){
+    var num1=3
+    var num2=5
+    var num3=5
+    var num4=7
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun value(){
+    println("compassionate")
+}
